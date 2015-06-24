5cf1b7d6189b79c129852803ffa1a9bbe8a8941a
♥ <hi@drwrf.com>
2015-06-23 22:53:40 -0400

    Configure postgres for use with Postgres.app.

--- config/dev.exs
+++ config/dev.exs
@@ -29,7 +29,7 @@ config :logger, :console, format: "[$level] $message\n"
 # Configure your database
 config :auth, Auth.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "",
+  password: nil,
   database: "auth_dev",
   size: 10 # The amount of database connections in the pool
