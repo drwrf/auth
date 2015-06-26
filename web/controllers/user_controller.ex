c49097932215c1eecdaacbf31c921b931a11ac23
♥ <hi@drwrf.com>
2015-06-25 23:41:01 -0400

    Add basic user response.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -19,17 +19,7 @@ defmodule Auth.UserController do
     end
   end
 
-  def show(conn, _params) do
-    json conn, %{
-      id: 1,
-      email: "hi@drwrf.com",
-      username: "hi@drwrf.com",
-      is_verified: true,
-      is_deleted: false,
-      is_recoverable: true,
-      created_at: "2015-06-21T18:24:18+00:00",
-      updated_at: "2015-06-21T18:24:18+00:00",
-      last_login: nil,
-    }
+  def show(conn, %{"id" => id, "format" => format}) do
+    json conn, Repo.get_by(User, id: id)
   end
 end
