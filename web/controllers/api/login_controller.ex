a550c0c3fc9c7a25facdf0aa32f6a4b060421dde
♥ <hi@drwrf.com>
2015-07-11 14:07:08 -0400

    Simplify controller logic.

--- web/controllers/api/login_controller.ex
+++ web/controllers/api/login_controller.ex
@@ -9,46 +9,11 @@ defmodule Auth.Api.LoginController do
   plug :action
 
   def create(conn, params) do
-    user = retrieve_user(conn, params)
-    login = create_login(conn, user)
+    login = retrieve_user(conn, params)
+         |> check_password(conn, params)
+         |> to_login(conn, params)
 
-    conn
-    |> check_password(user, params)
-    |> persist_login(login, params)
-  end
-
-  defp retrieve_user(conn, params) do
-    query = assoc(conn.assigns.org, :users)
-
-    # For now we only allow logins on username OR email, never
-    # both at the same time. This may be fine but it's better
-    # to be sure first before we expose it.
-    user = case params do
-      %{"email"    => e} -> Repo.get_by(query, email: e)
-      %{"username" => u} -> Repo.get_by(query, username: u)
-      __________________ -> nil
-    end
-  end
-
-  defp create_login(conn, user) do
-    case user do
-      nil -> nil
-      ___ -> Login.changeset(%Login{}, %{
-        "org_id" => conn.assigns.org.id,
-        "user_id" => user.id,
-      })
-    end
-  end
-
-  defp check_password(conn, user, params) do
-    case User.valid_password?(user, params) do
-      false -> conn |> send_resp(400, '') |> halt
-      true  -> conn
-    end
-  end
-
-  def persist_login(conn, login, params) do
-    if login.valid? do
+    if login && login.valid? do
       login = Repo.insert!(login)
 
       conn
@@ -57,6 +22,36 @@ defmodule Auth.Api.LoginController do
     else
       conn
       |> send_resp(400, '')
+    end
+  end
+
+  defp retrieve_user(conn, params) do
+    query = assoc(conn.assigns.org, :users)
+
+    # For now we only allow logins on username OR email, never
+    # both at the same time. This may be fine but it's better
+    # to be sure first before we expose it.
+    case params do
+      %{"email"    => e} -> Repo.get_by(query, email: e)
+      %{"username" => u} -> Repo.get_by(query, username: u)
+      __________________ -> nil
+    end
+  end
+
+  defp check_password(user, conn, params) do
+    case User.valid_password?(user, params["password"]) do
+      true  -> user
+      false -> nil
+    end
+  end
+
+  defp to_login(user, conn, params) do
+    case user do
+      nil -> nil
+      ___ -> Login.changeset(%Login{}, %{
+        "org_id" => conn.assigns.org.id,
+        "user_id" => user.id,
+      })
     end
   end
 end
